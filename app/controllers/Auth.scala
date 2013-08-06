5575d2556a05934f9380c24303eb9897f07558dc
Arya Irani <arya.irani@gmail.com>
2013-08-06 18:25:11 -0500

    naming consistency

--- app/controllers/Auth.scala
+++ app/controllers/Auth.scala
@@ -1,21 +1,21 @@
-    package controllers
+package controllers
 
-    import model._
-    import scala.slick.session.Database.threadLocalSession
-    import com.typesafe.slick.driver.oracle.OracleDriver.simple._
+import model._
+import scala.slick.session.Database.threadLocalSession
+import com.typesafe.slick.driver.oracle.OracleDriver.simple._
 
 
-    object Auth {
-      def auth(username: String, password: String): Boolean = {
-        val q = for {
-          (u,s) <- User leftJoin Company on (_.agencyId is _.id)
-          if u.name.toLowerCase === username.toLowerCase.trim && u.password === fn_hash(password)
-        } yield (s.dataOutOfDate.?.getOrElse(false))
+object Auth {
+  def auth(username: String, password: String): Boolean = {
+    val q = for {
+      (u,s) <- User leftJoin Agency on (_.agencyId is _.id)
+      if u.name.toLowerCase === username.toLowerCase.trim && u.password === fn_hash(password)
+    } yield (s.dataOutOfDate.?.getOrElse(false))
 
-        db withSession {
-          q.list.map {
-            case (agencyOutOfDate) => !agencyOutOfDate
-          }.headOption.getOrElse(false)
-        }
-      }
+    db withSession {
+      q.list.map {
+        case (agencyOutOfDate) => !agencyOutOfDate
+      }.headOption.getOrElse(false)
     }
+  }
+}
